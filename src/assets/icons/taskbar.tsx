6764fd71ca5dc398dbda611b9f958fae2090bb0f
Filipsys <filipsysak@gmail.com>
2025-08-07 23:59:29 +0200

    3 new icons

--- src/assets/icons/taskbar.tsx
+++ src/assets/icons/taskbar.tsx
@@ -116,3 +116,232 @@ export const lockIcon = () => (
     <path d="M14 26V28H16V26H14Z" fill="#808000" />
   </svg>
 );
+
+export const FlashlightIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6 2V0H8V2H10V4H12V6H14V8H16V10H14V8H12V6H10V4H8V2H6Z"
+      fill="#838383"
+    />
+    <path d="M12 14V12H14V14H12Z" fill="#838383" />
+    <path d="M10 22V20H12V22H14V20H16V22H14V24H12V22H10Z" fill="#838383" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M4 0H2V2H0V4H2V2H4V0ZM0 6V8H2V10H4V12H6V14H8V16H10V14H8V12H6V10H4V8H2V6H0Z"
+      fill="#838383"
+    />
+    <path d="M2 4H0V6H2V4Z" fill="#0000AA" />
+    <path d="M4 0V2H6V0H4Z" fill="#0000AA" />
+    <path d="M14 22V24H16V22H14Z" fill="#0000AA" />
+    <path d="M12 20V22H14V20H12Z" fill="#0000AA" />
+    <path d="M10 14V16V20H12V14H10Z" fill="#0000AA" />
+    <path d="M8 12V14H10V12H8Z" fill="#0000AA" />
+    <path d="M6 10V12H8V10H6Z" fill="#0000AA" />
+    <path d="M4 8V10H6V8H4Z" fill="#0000AA" />
+    <path d="M2 6V8H4V6H2Z" fill="#0000AA" />
+    <path d="M14 8H12V10H14V8Z" fill="#0000AA" />
+    <path d="M12 6H10V8H12V6Z" fill="#0000AA" />
+    <path d="M10 4H8V6H10V4Z" fill="#0000AA" />
+    <path d="M8 2H6V4H8V2Z" fill="#0000AA" />
+    <path d="M14 14H12V20H14H16V18V16H14V14Z" fill="#0092AA" />
+    <path d="M12 14V12H10V14H12Z" fill="#0092AA" />
+    <path d="M8 10V12H10V10H8Z" fill="#0092AA" />
+    <path d="M6 8V10H8V8H6Z" fill="#0092AA" />
+    <path d="M4 6V8H6V6H4Z" fill="#0092AA" />
+    <path d="M2 4V6H4V4H2Z" fill="#0092AA" />
+    <path d="M4 2H2V4H4V2Z" fill="#0000FF" />
+    <path d="M6 2H4V4V6H6V4V2Z" fill="#00FFFF" />
+    <path d="M6 6V8H8V6H6Z" fill="#00FFFF" />
+    <path d="M8 8V10H10V8H8Z" fill="#00FFFF" />
+    <path d="M10 10V12H12V10H10Z" fill="#00FFFF" />
+    <path d="M12 12H14V10H12V12Z" fill="white" />
+    <path d="M12 8H10V10H12V8Z" fill="white" />
+    <path d="M10 6H8V8H10V6Z" fill="white" />
+    <path d="M8 4H6V6H8V4Z" fill="white" />
+    <path d="M16 10H14V12H16H18H20V10H16Z" fill="#0000AA" />
+    <path d="M20 12V14H22V12H20Z" fill="#0000AA" />
+    <path d="M22 14V16H24V14H22Z" fill="#0000AA" />
+    <path d="M20 10V12H22V10H20Z" fill="#838383" />
+    <path d="M22 12V14H24V12H22Z" fill="#838383" />
+    <path d="M22 14H20V16H22V14Z" fill="#838383" />
+    <path d="M14 12V14H16V12H14Z" fill="white" />
+    <path d="M16 14V16H18V14H16Z" fill="white" />
+    <path d="M20 14V12H18V14V16H20V14Z" fill="#00FFFF" />
+    <path d="M18 16H16V18H18V16Z" fill="#00FFFF" />
+    <path d="M14 14V16H16V14H14Z" fill="#00FFFF" />
+    <path d="M16 12V14H18V12H16Z" fill="#0092AA" />
+    <path d="M16 18V20H18V18H16Z" fill="#0000FF" />
+    <path d="M20 16H18V18H20V16Z" fill="#0000FF" />
+    <path d="M18 18V20H20V18H18Z" fill="#FFFF00" />
+    <path d="M22 16H20V18V20H22V18V16Z" fill="#808000" />
+    <path d="M16 20V22H18H20V20H18H16Z" fill="#808000" />
+    <path d="M32 18V16H30V18H32Z" fill="#FFFF00" />
+    <path d="M32 22V20H30V22H32Z" fill="#FFFF00" />
+    <path d="M32 26V24H30V26H32Z" fill="#FFFF00" />
+    <path d="M32 30V28H30V30H32Z" fill="#FFFF00" />
+    <path d="M28 32H30V30H28V32Z" fill="#FFFF00" />
+    <path d="M28 28H26V30H28V28Z" fill="#FFFF00" />
+    <path d="M24 32H26V30H24V32Z" fill="#FFFF00" />
+    <path d="M22 28V30H24V28H22Z" fill="#FFFF00" />
+    <path d="M20 32H22V30H20V32Z" fill="#FFFF00" />
+    <path d="M18 28V30H20V28H18Z" fill="#FFFF00" />
+    <path d="M16 30V32H18V30H16Z" fill="#FFFF00" />
+    <path d="M16 26V28H18V26H16Z" fill="#FFFF00" />
+    <path d="M18 24V26H20V24H18Z" fill="#FFFF00" />
+    <path d="M20 26V28H22V26H20Z" fill="#FFFF00" />
+    <path d="M24 26V28H26V26H24Z" fill="#FFFF00" />
+    <path d="M30 26H28V28H30V26Z" fill="#FFFF00" />
+    <path d="M28 24H26V26H28V24Z" fill="#FFFF00" />
+    <path d="M22 24V26H24V24H22Z" fill="#FFFF00" />
+    <path d="M30 22H28V24H30V22Z" fill="#FFFF00" />
+    <path d="M24 22V24H26V22H24Z" fill="#FFFF00" />
+    <path d="M28 20H26V22H28V20Z" fill="#FFFF00" />
+    <path d="M30 18H28V20H30V18Z" fill="#FFFF00" />
+    <path d="M28 16H26V18H28V16Z" fill="#FFFF00" />
+    <path d="M24 18V20H26V18H24Z" fill="#FFFF00" />
+    <path d="M22 20V22H24V20H22Z" fill="#FFFF00" />
+    <path d="M32 20V18H30V20H32Z" fill="white" />
+    <path d="M32 24V22H30V24H32Z" fill="white" />
+    <path d="M32 28V26H30V28H32Z" fill="white" />
+    <path d="M30 32H32V30H30V32Z" fill="white" />
+    <path d="M30 28H28V30H30V28Z" fill="white" />
+    <path d="M26 32H28V30H26V32Z" fill="white" />
+    <path d="M24 28V30H26V28H24Z" fill="white" />
+    <path d="M22 32H24V30H22V32Z" fill="white" />
+    <path d="M18 32H20V30H18V32Z" fill="white" />
+    <path d="M20 28V30H22V28H20Z" fill="white" />
+    <path d="M16 28V30H18V28H16Z" fill="white" />
+    <path d="M18 26V28H20V26H18Z" fill="white" />
+    <path d="M22 26V28H24V26H22Z" fill="white" />
+    <path d="M26 26V28H28V26H26Z" fill="white" />
+    <path d="M30 24H28V26H30V24Z" fill="white" />
+    <path d="M24 24V26H26V24H24Z" fill="white" />
+    <path d="M20 24V26H22V24H20Z" fill="white" />
+    <path d="M18 24H16V26H18V24Z" fill="white" />
+    <path d="M20 22H18V24H20V22Z" fill="white" />
+    <path d="M22 22V24H24V22H22Z" fill="white" />
+    <path d="M28 22H26V24H28V22Z" fill="white" />
+    <path d="M30 20H28V22H30V20Z" fill="white" />
+    <path d="M24 20V22H26V20H24Z" fill="white" />
+    <path d="M22 18V20H24V18H22Z" fill="white" />
+    <path d="M26 16H24V18H26V16Z" fill="white" />
+    <path d="M30 16H28V18H30V16Z" fill="white" />
+    <path d="M20 22V24H22V22H20Z" fill="#FFFF00" />
+    <path d="M28 18H26V20H28V18Z" fill="white" />
+    <path d="M22 20H20V22H22V20Z" fill="white" />
+  </svg>
+);
+
+export const TilesIcon = () => (
+  <svg
+    width="32"
+    height="12"
+    viewBox="0 0 32 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M2 2V10H4V8V4H8H10V2H2Z" fill="white" />
+    <path d="M12 2V10H14V8V4H18H20V2H12Z" fill="white" />
+    <path d="M30 2H22V10H24V8V4H28H30V2Z" fill="white" />
+    <path d="M24 10H30V4H28V8H24V10Z" fill="#838383" />
+    <path d="M14 10H20V4H18V8H14V10Z" fill="#838383" />
+    <path d="M4 10H10V4H8V8H4V10Z" fill="#838383" />
+    <path d="M4 4V8H8V4H4Z" fill="#C1C1C1" />
+    <path d="M18 4H14V8H18V4Z" fill="#C1C1C1" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 0V12H32V0H0ZM2 10V2H10V4V10H4H2ZM12 2V10H14H20V4V2H12ZM22 2H30V4V10H24H22V2Z"
+      fill="#0000FF"
+    />
+    <path d="M24 4V8H28V4H24Z" fill="#C1C1C1" />
+  </svg>
+);
+
+export const ComputerXIcon = () => (
+  <svg
+    width="30"
+    height="32"
+    viewBox="0 0 30 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M22 24H14V26H22V24Z" fill="#838383" />
+    <path d="M14 28H22V26H14V28Z" fill="#C1C1C1" />
+    <path d="M4 28H8V26H4V28Z" fill="#C1C1C1" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M2 28V24H4V26V28H2ZM8 26V28H14V26V24H8V26ZM22 28H24V24H22V26V28Z"
+      fill="white"
+    />
+    <path d="M8 24H4V26H8V24Z" fill="#00FF00" />
+    <path d="M26 18V20H28V18H26Z" fill="black" />
+    <path d="M28 18H30V16H28V18Z" fill="black" />
+    <path d="M28 16H30V14H28V16Z" fill="#800000" />
+    <path d="M26 14H28V12H26V14Z" fill="#800000" />
+    <path d="M24 12H26V10H24V12Z" fill="#800000" />
+    <path d="M24 10V8H22V10H24Z" fill="#800000" />
+    <path d="M18 4V2H16V4H18Z" fill="#800000" />
+    <path d="M14 2H16V0H14V2Z" fill="#800000" />
+    <path d="M14 16V18H16V16H14Z" fill="black" />
+    <path d="M12 18V20H14V18H12Z" fill="black" />
+    <path d="M10 20V22H12V20H10Z" fill="black" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M20 20V22H12V20H14H20ZM4 20H8V22H4V20Z"
+      fill="#838383"
+    />
+    <path d="M8 20V22H10V20H8Z" fill="#FF0000" />
+    <path d="M8 20H10H12V18H10H8V20Z" fill="#FF0000" />
+    <path d="M16 14V16H18V14H16Z" fill="black" />
+    <path d="M18 12V14H20V12H18Z" fill="black" />
+    <path d="M16 16V18H24V16H22H18H16Z" fill="#C1C1C1" />
+    <path d="M2 4V18H8H10V16H4V6H12V4H10H2Z" fill="white" />
+    <path d="M26 18H28V16V14H26V18Z" fill="#FF0000" />
+    <path d="M20 8H22V6H20V8Z" fill="#FF0000" />
+    <path d="M24 8H26V6H24V8Z" fill="black" />
+    <path d="M26 4V6H28V4H26Z" fill="black" />
+    <path d="M28 2V4H30V2H28Z" fill="black" />
+    <path d="M22 8H24V6H22V8Z" fill="#FF0000" />
+    <path d="M22 4V6H24V4H22Z" fill="#FF0000" />
+    <path d="M26 4H24V6H26V4Z" fill="#FF0000" />
+    <path d="M26 4V2H24V4H26Z" fill="#FF0000" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M28 20V32H2V30H26.0595L26 22H22V20H26H28ZM26 8V6H28V12H26V10V8Z"
+      fill="#838383"
+    />
+    <path d="M28 2H26V4H28V2Z" fill="#FF0000" />
+    <path d="M20 6H22V4H20V6Z" fill="white" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M26 14V12H24V10H22V8H20V6H18V4H16H12V6H14V8H16V12H14V14H12V16H10V18H12H14V16H16V14H18V12H20V14H22V16H24V18H26V14Z"
+      fill="#FF0000"
+    />
+    <path d="M20 4H18V6H20V4Z" fill="#800000" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 2H10V4H2V18H8V20H4V22H8H10H12H20V20H14V18H16H24H26V20H22V22H26L26.0595 30H2H0V22H2V20H0V2ZM20 4H18V2H24V4H22H20ZM2 24V28H4H8H14H22H24V24H22H14H8H4H2Z"
+      fill="#0000AA"
+    />
+    <path d="M16 4V2H14H10V4H12H16Z" fill="#FF0000" />
+    <path d="M14 2V0H10V2H14Z" fill="#FF0000" />
+    <path d="M12 14H14V12H16V8H14H10V10H8V12H6V14H12Z" fill="#0092AA" />
+    <path d="M6 8V12H8V10H10V8H6Z" fill="#00FFFF" />
+    <path d="M10 16H12V14H6V12V8H10H14V6H12H4V16H10Z" fill="#0000FF" />
+    <path d="M22 14H20H18V16H22V14Z" fill="#0000FF" />
+    <path d="M24 10H26V8H24V10Z" fill="#0000AA" />
+  </svg>
+);
