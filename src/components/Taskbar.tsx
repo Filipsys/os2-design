a582e840cc92dc0d09e30ad6a5cefbf3f695a95a
Filipsys <filipsysak@gmail.com>
2025-08-08 00:12:28 +0200

    Added vite svgr plugin, changed svgs into files, and added a few new icons

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,5 +1,11 @@
 import type { JSX } from "react";
-import { lockIcon, switchIcon } from "../assets/icons/taskbar";
+import BoxIcon from "../assets/icons/box-icon.svg?react";
+import CanIcon from "../assets/icons/can-icon.svg?react";
+import ComputerXIcon from "../assets/icons/computer-x-icon.svg?react";
+import FlashlightIcon from "../assets/icons/flashlight-icon.svg?react";
+import LockIcon from "../assets/icons/lock-icon.svg?react";
+import SwitchIcon from "../assets/icons/switch-icon.svg?react";
+import TilesIcon from "../assets/icons/tiles-icon.svg?react";
 
 const Button = (props: { icon: React.FC }) => (
   <div className="relative p-0.5 h-full aspect-[6/5] border-2 border-r-black border-b-black border-t-gray-200 border-l-gray-200 active:border-t-black active:border-l-black  active:border-b-gray-200 active:translate-x-[1px] active:translate-y-[1px] *:size-[21px]">
@@ -19,7 +25,7 @@ export const Taskbar = () => (
       <img src="oswarp.png" alt="oswarp-logo" className="h-3" />
     </ButtonImg>
 
-    <Button icon={switchIcon} />
-    <Button icon={lockIcon} />
+    <Button icon={SwitchIcon} />
+    <Button icon={LockIcon} />
   </div>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), svgr()],
 });
