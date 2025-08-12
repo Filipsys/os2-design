e338a2a73cd2c3d07b17c87c48ca30de18b00f7f
Filipsys <filipsysak@gmail.com>
2025-08-12 23:51:10 +0200

    Basic app grid

---
+++ src/components/AppGrid.tsx
@@ -0,0 +1,3 @@
+export const AppGrid = () => (
+  <div className="grid grid-cols-6 grid-rows-7"></div>
+);
--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,7 +1,10 @@
+import { AppGrid } from "./AppGrid";
 import { Taskbar } from "./Taskbar";
 
 export const Desktop = () => (
   <div className="relative aspect-[5/4] h-2/3 overflow-hidden select-none bg-white">
+    <AppGrid />
+
     <Taskbar />
 
     <img src="os2warp-bg.png" alt="background" className="h-full" />
